176dbe7af67501e3e065a0991f72bce34bdaee9b
Piotr Zieliński <piotrekzie100@gmail.com>
2023-04-01 20:27:46 +0200

    Unify dimensions for intermediates in DIR

--- src/models/components/encode/encoder.py
+++ src/models/components/encode/encoder.py
@@ -1,6 +1,7 @@
 """DIR encoder."""
+import math
 from copy import deepcopy
-from typing import Optional, Tuple, Type, Union
+from typing import Dict, Optional, Tuple, Union
 
 import torch
 import torch.nn as nn
@@ -12,6 +13,104 @@ from src.models.components.encode.parse import Backbone, Head, Neck, parse_yolov
 from src.models.components.encode.rnn import PackedSequence, SeqEncoder, packed_forward
 from src.models.components.encode.what import WhatEncoder
 from src.models.components.latents import DIRLatents
+
+
+class Mixer(nn.Module):
+    """Module for mixing multi-level features."""
+
+    def __init__(self, anchors: Dict[int, int], out_channels: Dict[int, int]):
+        super().__init__()
+
+        self.anchors = anchors
+        self._default_channels = out_channels
+        n_channels = min([self._default_channels[key] for key in self.anchors])
+        self.out_channels = {key: n_channels for key in self.anchors}
+
+        self._downscalers = self._build_downscalers()
+        self._mixers = self._build_mixers()
+
+    @staticmethod
+    def _build_conv_block(in_channels: int, out_channels: int, **conv2d_kwargs):
+        """Build convolutional block."""
+        return nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, **conv2d_kwargs, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU(True),
+        )
+
+    def _build_downscalers(self):
+        """Build downscalers for features."""
+        downscalers = {}
+        for key in self.anchors:
+            in_channels = self._default_channels[key]
+            out_channels = self.out_channels[key]
+            power = int(math.log2(in_channels / out_channels))
+            downscaler = []
+            for _ in range(power):
+                downscaler.append(
+                    self._build_conv_block(
+                        in_channels,
+                        in_channels // 2,
+                        kernel_size=3,
+                        padding=1,
+                        stride=2,
+                    )
+                )
+                in_channels //= 2
+            downscalers[key] = nn.Sequential(*downscaler)
+        return downscalers
+
+    def _build_mixers(self) -> nn.ModuleDict:
+        """Build mixers for features."""
+        mixers = {}
+        iterable = list(self.anchors.keys())
+        channels = {k: self.out_channels[k] for k in iterable}
+
+        for larger, smaller in zip(iterable, iterable[1:]):
+            larger_channels = self.out_channels[larger]
+            smaller_channels = self.out_channels[smaller]
+
+            downscaled_channels = smaller_channels // 2
+            downscaler = self._build_conv_block(
+                larger_channels, downscaled_channels, kernel_size=3, padding=1, stride=2
+            )
+
+            upscaled_channels = larger_channels // 2
+            upscaler = self._build_conv_block(
+                smaller_channels, upscaled_channels, kernel_size=4, stride=2, padding=1
+            )
+
+            mixers[f"{larger},{smaller}"] = nn.ModuleList([downscaler, upscaler])
+
+            channels[larger] += upscaled_channels
+            channels[smaller] = downscaled_channels
+
+        for idx, in_channels in channels.items():
+            out_channels = self.out_channels[idx]
+            mixers[str(idx)] = self._build_conv_block(
+                in_channels, out_channels, kernel_size=1
+            )
+
+        return nn.ModuleDict(mixers)
+
+    def forward(self, features: Dict[int, torch.Tensor]) -> Dict[int, torch.Tensor]:
+        ret = {}
+        for key in self.anchors:
+            ret[key] = self._downscalers[key](features[key])
+
+        iterable = list(ret.items())
+        for (l, l_features), (s, s_features) in zip(iterable, iterable[1:]):
+            downscaler, upscaler = self._mixers[f"{l},{s}"]
+            downscaled = downscaler(l_features)
+            upscaled = upscaler(s_features)
+
+            ret[l] = torch.cat([ret[l], upscaled], dim=1)
+            ret[s] = torch.cat([ret[s], downscaled], dim=1)
+
+        for idx, features in ret.items():
+            ret[idx] = self._mixers[str(idx)](features)
+
+        return ret
 
 
 class Encoder(nn.Module):
@@ -78,17 +177,19 @@ class Encoder(nn.Module):
         self.where_head = WhereHead()
         self.present_head = PresentHead(filter_classes)
 
+        self.mixer = Mixer(self.head.num_anchor, self.neck.out_channels)
+
         self.what_enc = what_enc or WhatEncoder(
             latent_dim=z_what_size,
             num_hidden=z_what_hidden,
             anchors=self.head.num_anchors,
-            out_channels=self.neck.out_channels,
+            out_channels=self.mixer.out_channels,
             scale_const=z_what_scale_const,
         )
         self.what_enc.requires_grad_(train_what)
         self.depth_enc = depth_enc or DepthEncoder(
             anchors=self.head.num_anchors,
-            out_channels=self.neck.out_channels,
+            out_channels=self.mixer.out_channels,
             scale_const=z_depth_scale_const,
         )
         self.depth_enc.requires_grad_(train_depth)
@@ -153,6 +254,8 @@ class Encoder(nn.Module):
             features = self.cloned_backbone(images)
         if self.cloned_neck is not None:
             intermediates = self.cloned_neck(features)
+
+        intermediates = self.mixer(intermediates)
 
         z_what = self.what_enc(intermediates)
         z_depth = self.depth_enc(intermediates)
